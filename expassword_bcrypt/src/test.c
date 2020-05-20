e2943d52bfe508bd7cac651de2540ac0bf8777f4
julp <e2c2be7ed0f2f336@gmail.com>
2020-05-20 19:00:22 +0200

    better assertions

--- expassword_bcrypt/src/test.c
+++ expassword_bcrypt/src/test.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <getopt.h>
 
 #define ARRAY_SIZE(array) \
     (sizeof(array) / sizeof((array)[0]))
@@ -12,19 +13,54 @@
 #define STR_SIZE(str) \
     (ARRAY_SIZE(str))
 
+#define RED(str) "\33[1;31m" str "\33[0m"
+#define GREEN(str) "\33[1;32m" str "\33[0m"
+
+#define ASSERT(expr, description) \
+    do { \
+        if (expr) { \
+            if (verbose) { \
+                fprintf(stderr, "[ " GREEN("PASS") " ] " description " (%d)\n", __LINE__); \
+            } \
+        } else { \
+            retval = EXIT_FAILURE; \
+            fprintf(stderr, "[ " RED("FAIL") " ] " description " => test %s failed at line %d\n", #expr, __LINE__); \
+        } \
+    } while (0);
+
 extern uint8_t *decode_base64(const uint8_t *data, const uint8_t * const data_end, uint8_t *buffer, const uint8_t * const buffer_end);
 
 extern uint8_t *bcrypt_full_parse_hash(uint8_t *hash, const uint8_t *hash_end, int *minor, int *cost, uint8_t *raw_salt, const uint8_t * const raw_salt_end);
 extern bool bcrypt_hash(const uint8_t *password, const uint8_t * const password_end, const uint8_t *raw_salt, const uint8_t * const raw_salt_end, uint8_t *hash, const uint8_t * const hash_end, int minor, int cost);
 
-int main(void)
+static char optstr[] = "v";
+
+static struct option long_options[] =
 {
-    int retval;
+    {"verbose", no_argument, NULL, 'v'},
+    {NULL,      no_argument, NULL, 0}
+};
+
+int main(int argc, char **argv)
+{
+    uint8_t *p;
+    int c, retval, verbose;
     uint8_t hash[128];
     uint8_t password[] = "U*U";
     uint8_t salt[] = "$2a$05$CCCCCCCCCCCCCCCCCCCCC.";
 
+    verbose = 0;
     retval = EXIT_SUCCESS;
+    while (-1 != (c = getopt_long(argc, argv, optstr, long_options, NULL))) {
+        switch (c) {
+          case 'v':
+              verbose++;
+              break;
+          default:
+              // NOP
+              break;
+        }
+    }
 
     if (bcrypt_hash(password, password + STR_SIZE(password), salt, salt + STR_SIZE(salt), hash, hash + STR_SIZE(hash), 'a', 5)) {
         //retval = 0 == strcmp((char *) hash, "$2a$05$CCCCCCCCCCCCCCCCCCCCC.E5YPO9kmyuRGyh0XouQYb4YMJKvyOeW") ? EXIT_SUCCESS : EXIT_FAILURE;
@@ -37,75 +73,44 @@ int main(void)
 
     // normal case without any additional space
     {
-        uint8_t *p, data[22] = "CCCCCCCCCCCCCCCCCCCCC.", buffer[16], expected[16] = {0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10};
+        uint8_t data[22] = "CCCCCCCCCCCCCCCCCCCCC.", buffer[16], expected[16] = {0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10};
 
-        if (NULL == (p = decode_base64(data, data + STR_SIZE(data), buffer, buffer + STR_SIZE(buffer)))) {
-            retval = EXIT_FAILURE;
-            fprintf(stderr, "decode_base64 failed to decode data (%d)\n", __LINE__);
-        } else {
-            if (p != buffer + STR_SIZE(buffer)) {
-                retval = EXIT_FAILURE;
-                fprintf(stderr, "decode_base64 failed to return correct position from the input string (%d)\n", __LINE__);
-            }
-            if (0 != memcmp(buffer, expected, STR_SIZE(expected))) {
-                retval = EXIT_FAILURE;
-                fprintf(stderr, "decode_base64 failed to correctly decode input string (%d)\n", __LINE__);
-            }
-        }
+        p = decode_base64(data, data + STR_SIZE(data), buffer, buffer + STR_SIZE(buffer));
+        ASSERT(NULL != p, "decode_base64 failed to decode data");
+        ASSERT(p == buffer + STR_SIZE(expected), "decode_base64 failed to return correct position from the input string");
+        ASSERT(0 == memcmp(buffer, expected, STR_SIZE(expected)), "decode_base64 failed to correctly decode input string");
     }
     // normal case with larger output buffer
     {
-        uint8_t *p, data[22] = "CCCCCCCCCCCCCCCCCCCCC.", buffer[32], expected[16] = {0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10};
+        uint8_t data[22] = "CCCCCCCCCCCCCCCCCCCCC.", buffer[32], expected[16] = {0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10};
 
-        if (NULL == (p = decode_base64(data, data + STR_SIZE(data), buffer, buffer + STR_SIZE(buffer)))) {
-            retval = EXIT_FAILURE;
-            fprintf(stderr, "decode_base64 failed to decode data (%d)\n", __LINE__);
-        } else {
-            if (p != buffer + STR_SIZE(expected)) {
-                retval = EXIT_FAILURE;
-                fprintf(stderr, "decode_base64 failed to return correct position from the input string (%d)\n", __LINE__);
-            }
-            if (0 != memcmp(buffer, expected, STR_SIZE(expected))) {
-                retval = EXIT_FAILURE;
-                fprintf(stderr, "decode_base64 failed to correctly decode input string (%d)\n", __LINE__);
-            }
-        }
+        p = decode_base64(data, data + STR_SIZE(data), buffer, buffer + STR_SIZE(buffer));
+        ASSERT(NULL != p, "decode_base64 failed to decode data");
+        ASSERT(p == buffer + STR_SIZE(expected), "decode_base64 failed to return correct position from the input string");
+        ASSERT(0 == memcmp(buffer, expected, STR_SIZE(expected)), "decode_base64 failed to correctly decode input string");
     }
     // normal case without any additional space but a \0 terminated input string
     {
-        uint8_t *p, data[] = "CCCCCCCCCCCCCCCCCCCCC.", buffer[16], expected[16] = {0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10};
+        uint8_t data[] = "CCCCCCCCCCCCCCCCCCCCC.", buffer[16], expected[16] = {0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10, 0x41, 0x04, 0x10};
 
-        if (NULL == (p = decode_base64(data, data + STR_LEN(data), buffer, buffer + STR_SIZE(buffer)))) {
-            retval = EXIT_FAILURE;
-            fprintf(stderr, "decode_base64 failed to decode data (%d)\n", __LINE__);
-        } else {
-            if (p != buffer + STR_SIZE(expected)) {
-                retval = EXIT_FAILURE;
-                fprintf(stderr, "decode_base64 failed to return correct position from the input string (%d)\n", __LINE__);
-            }
-            if (0 != memcmp(buffer, expected, STR_SIZE(expected))) {
-                retval = EXIT_FAILURE;
-                fprintf(stderr, "decode_base64 failed to correctly decode input string (%d)\n", __LINE__);
-            }
-        }
+        p = decode_base64(data, data + STR_LEN(data), buffer, buffer + STR_SIZE(buffer));
+        ASSERT(NULL != p, "decode_base64 failed to decode data");
+        ASSERT(p == buffer + STR_SIZE(expected), "decode_base64 failed to return correct position from the input string");
+        ASSERT(0 == memcmp(buffer, expected, STR_SIZE(expected)), "decode_base64 failed to correctly decode input string");
     }
     // output buffer too small
     {
         uint8_t data[22] = "CCCCCCCCCCCCCCCCCCCCC.", buffer[15];
 
-        if (NULL != decode_base64(data, data + STR_SIZE(data), buffer, buffer + STR_SIZE(buffer))) {
-            retval = EXIT_FAILURE;
-            fprintf(stderr, "decode_base64 failed to report unsufficient space for output buffer (%d)\n", __LINE__);
-        }
+        p = decode_base64(data, data + STR_SIZE(data), buffer, buffer + STR_SIZE(buffer));
+        ASSERT(NULL == p, "decode_base64 failed to report unsufficient space for output buffer");
     }
     // invalid input string
     {
         uint8_t data[22] = "CCCCCCCCCCC+CCCCCCCCC.", buffer[16];
 
-        if (NULL != decode_base64(data, data + STR_SIZE(data), buffer, buffer + STR_SIZE(buffer))) {
-            retval = EXIT_FAILURE;
-            fprintf(stderr, "decode_base64 failed to report invalid character (%d)\n", __LINE__);
-        }
+        p = decode_base64(data, data + STR_SIZE(data), buffer, buffer + STR_SIZE(buffer));
+        ASSERT(NULL == p, "decode_base64 failed to report invalid character");
     }
 
     return retval;
